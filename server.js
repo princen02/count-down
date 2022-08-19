2da57fd8be1e5bdeb4bd9ffaba9246b3dcd48cf8
Prince Ndebele <princendex460@hotmail.com>
2022-08-20 00:57:36 +0200

    the countdown timer is runner

--- server.js
+++ server.js
@@ -95,29 +95,6 @@ if (process.env.NODE_ENV !== 'production') {
     next()
   }
  
-    function countdown() {
-      const newYearsDate = new Date(newYears);
-      const currentDate = new Date();
-
-      const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
-
-      const days = Math.floor(totalSeconds / 3600 / 24);
-      const hours = Math.floor(totalSeconds / 3600) % 24;
-      const mins = Math.floor(totalSeconds / 60) % 60;
-      const seconds = Math.floor(totalSeconds) % 60;
-
-      daysEl.innerHTML = days;
-      hoursEl.innerHTML = formatTime(hours);
-      minsEl.innerHTML = formatTime(mins);
-      secondsEl.innerHTML = formatTime(seconds);
-  }
-
-  function formatTime(time) {
-      return time < 10 ? (`0${time}`) : time;
-  }
-
-  countdown ();
-
-  setInterval(countdown, 1000);
+   
 
   app.listen(3000)
